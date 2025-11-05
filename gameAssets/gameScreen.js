af2ce4e53e7d3148e88c3764819a4a1b39ee9f79
constantin9845 <constantin.y@outlook.com>
2025-11-05 16:15:35 +0900

    added template for player movement with dynamic map animation

---
+++ gameAssets/gameScreen.js
@@ -0,0 +1,161 @@
+export class gameScreen{
+
+    constructor(){
+        this.SCALE = 2.5;
+        this.WIDTH = 16;
+        this.HEIGHT = 18;
+
+        this.scaled_width = this.SCALE * this.WIDTH;
+        this.scaled_height = this.SCALE * this.HEIGHT;
+
+        this.CYCLE_LOOP = [0, 1, 0, 2];
+        this.FACING_DOWN = 0;
+        this.FACING_UP = 1;
+        this.FACING_LEFT = 2;
+        this.FACING_RIGHT = 3;
+        this.FRAME_LIMIT = 12;
+        this.MOVEMENT_SPEED = 4;
+
+        this.canvas = document.getElementById('canvas');
+
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        this.ctx = this.canvas.getContext('2d');
+        this.keyPresses = {a:false, d:false};
+
+        this.CURRENT_DIR = this.FACING_DOWN;
+        this.CURRENT_LOOP_INDEX = 0;
+        this.framecount = 0;
+
+        this.X = this.canvas.width/2;
+        this.Y = this.canvas.height*0.94;
+
+        this.MID = this.canvas.width/2;
+        this.WALK_RANGE = this.canvas.width*2;
+        this.RANGE_MID = this.WALK_RANGE-this.MID;
+        this.WALK_X = this.X;
+
+        this.MOVE_BG = false;
+
+
+
+        this.spriteSheet = new Image();
+
+        this.isLoaded = false;
+
+        this.spriteSheet.onload = () => {
+            this.isLoaded = true;
+            console.log("Assets loaded. Game starting...");
+            this.startLoop();
+        };
+    }
+
+    getImage(){return this.spriteSheet;}
+
+    loadImage(image){
+        image.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
+        image.onload = ()=>{
+            window.requestAnimationFrame(this.gameLoop);
+        }
+    }
+
+    keyDownListener(key){
+        this.keyPresses[key] = true;
+    }
+
+    keyUpListener(key){
+        this.keyPresses[key] = false;
+    }
+
+    drawImage(frameX, frameY, canvasX, canvasY){
+        this.ctx.drawImage(
+            this.spriteSheet,
+            frameX * this.WIDTH,
+            frameY * this.HEIGHT,
+            this.WIDTH,
+            this.HEIGHT,
+            canvasX,
+            canvasY,
+            this.scaled_width,
+            this.scaled_height
+        )
+    }
+
+    moveCharacter(deltaX, direction){
+
+        console.log(`X: ${this.WALK_X}`);
+        console.log(this.MID);
+        console.log(this.WALK_RANGE-this.MID);
+
+        // rules to move background
+        if(this.WALK_X > this.MID && this.WALK_X < (this.WALK_RANGE-this.MID)){
+            this.MOVE_BG = true;
+            console.log('MOVE BG!');
+        }
+        else{
+            this.MOVE_BG = false;
+        }
+
+        if(this.X + deltaX > 0 && this.X+this.scaled_width+deltaX < this.canvas.width){
+            
+            if(this.WALK_X > this.MID && this.WALK_X < (this.WALK_RANGE-this.MID)){
+                this.WALK_X += deltaX;
+            }
+            else{
+                this.WALK_X += deltaX;
+                this.X += deltaX;
+            }
+        }
+
+        this.CURRENT_DIR = direction;
+    }
+
+    gameLoop = () => {
+        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+
+        let hasMoved = false;
+
+        if(this.keyPresses.a){
+            this.moveCharacter(-this.MOVEMENT_SPEED, this.FACING_LEFT);
+            hasMoved = true;
+        }
+        else if(this.keyPresses.d){
+            this.moveCharacter(this.MOVEMENT_SPEED, this.FACING_RIGHT);
+            hasMoved = true;
+        }
+
+        if(hasMoved){
+            this.framecount++;
+            if(this.framecount >= this.FRAME_LIMIT){
+                this.framecount = 0;
+                this.CURRENT_LOOP_INDEX++;
+
+                if(this.CURRENT_LOOP_INDEX >= this.CYCLE_LOOP.length){
+                    this.CURRENT_LOOP_INDEX = 0;
+                }
+            }
+        }
+
+        if(!hasMoved){
+            this.CURRENT_LOOP_INDEX = 0;
+            this.CURRENT_DIR = this.FACING_DOWN;
+        }
+
+        this.drawImage(this.CYCLE_LOOP[this.CURRENT_LOOP_INDEX], this.CURRENT_DIR, this.X, this.Y);
+        window.requestAnimationFrame(this.gameLoop);
+    }
+
+    
+
+    startLoop() {
+        const loop = () => {
+            this.update();
+            this.draw();
+            window.requestAnimationFrame(loop);
+        };
+        window.requestAnimationFrame(loop);
+    }
+
+
+}
--- scripts/level.js
+++ scripts/level.js
@@ -1,1 +1,79 @@
+import { gameScreen } from '../gameAssets/gameScreen.js';
+const { ipcRenderer } = require('electron');
+
+document.body.width = window.innerWidth;
+document.body.height = window.innerHeight;
+
+let LEVEL = 0;
+let transform = 0;
+
+const game = new gameScreen();
+
+window.addEventListener('keydown', (e)=>{
+    e.preventDefault();
+
+    if(e.key == 'a' || e.key == 'd'){
+        game.keyDownListener(e.key);
+
+        if(game.MOVE_BG){
+            if(e.key == 'd'){
+                transform-=10;
+                document.getElementById('front_bg').style.transform = `translateX(${transform}px)`;
+            }
+            else{
+                transform+=10;
+                document.getElementById('front_bg').style.transform = `translateX(${transform}px)`;
+            }
+        }
+    }
+
+    if(e.key == 'Escape'){
+        ipcRenderer.send('level-selection');
+    }
+});
+
+window.addEventListener('keyup', (e)=>{
+    e.preventDefault();
+
+    if(e.key == 'a' || e.key == 'd'){
+        game.keyUpListener(e.key);
+    }
+});
+
+game.loadImage(game.getImage());
+
+constructLevel();
+
+
+const title = document.getElementById('title');
+const canvas = document.getElementById('canvas');
+
+async function constructLevel(){
+    
+    LEVEL = await waitForLevelReq();
+
+    title.innerHTML = `Level ${LEVEL}`;
+
+    document.body.style.backgroundImage = `url('./level_imgs/bg.jpg')`;
+
+    let front_bg = document.createElement('img');
+    front_bg.src = './level_imgs/front_bg.png';
+    front_bg.id = 'front_bg';
+
+    document.body.appendChild(front_bg);
+}
+
+function waitForLevelReq(){
+    return new Promise((resolve)=>{
+
+        const handler = (event, data) =>{
+            resolve(data);
+        }
+
+        ipcRenderer.once('res_current_level', handler);
+
+        ipcRenderer.send('get_current_level');
+    })
+}
+
 
